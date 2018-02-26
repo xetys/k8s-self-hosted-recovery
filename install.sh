5d019cfb5806e41e0ee02550be12d4e425bf7ef7
crmendes <carlos.rmendes@wedotechnologies.com>
2018-02-26 14:37:42 +0000

    fix mispelling User key value and add chmod to give execution permission to k8s-self-hosted-recover file

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@
 cp k8s-self-hosted-recover /usr/local/bin/
 cp k8s-self-hosted-recover.service /etc/systemd/system/
 
+chmod 755 /usr/local/bin/k8s-self-hosted-recover
+
 systemctl enable k8s-self-hosted-recover.service
